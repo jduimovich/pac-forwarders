610b2efaf4305c2a30afec8d08765f4a7d995ac6
John Duimovich <jduimovich@gmail.com>
2024-02-13 10:22:56 -0500

    spray proxy forwarder

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# pac-forwarders
-# pac-forwarders
+# pac-forwarders 
+
+This repo has a simple PaC forwarder for use with Github Apps
+
+It uses smee to create a fixed callback for your github app and spray proxy to forward to the actual pac backend
+
+The double forward is because smee does not correctly connect to the local pac handler so this is a quick workaround. 
+
+## Run 
+
+Modify the script `smee-local.cmd` -- windows CRC users need to 
+
--- smee-local.cmd
+++ smee-local.cmd
@@ -1,1 +1,12 @@
-smee -u https://smee.io/jduimovich-crc-machine -t http://localhost:9999
+
+# Start a smee forwarder to localhost:9999 and use spray proxy to forward it
+# the double forward is because smee does not actually connect to the local pac handler 
+# it seems to error, this is a quick workaround. 
+set PAC_CALLBACK=https://smee.io/jduimovich-crc-machine
+set PAC_HANDLER=https://pipelines-as-code-controller-openshift-pipelines.apps-crc.testing
+set SECURE=--insecure-skip-webhook-verify  --insecure-skip-tls-verify
+set PORT=9999
+
+echo "Smee in background window, remember to close when done."
+start cmd /min /c smee -u %PAC_CALLBACK% -t http://localhost:%PORT%
+..\sprayproxy\sprayproxy server %SECURE%  --port %PORT% -- %PAC_HANDLER% 
